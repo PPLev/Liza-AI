f28898f731bc19a7395eb40b9653a5f278b10e7f
Admin <sergo.p.987@gmail.com>
2024-03-22 23:35:24 +0300

    настройки силеро

--- plugins/plugin_silero.py
+++ plugins/plugin_silero.py
@@ -14,34 +14,46 @@ core = Core()
 
 silero_model = None
 is_mute = False
-
+model_settings = {}
 
 async def start(core: Core):
     manifest = {
         "name": "Плагин генерации речи с помощью silero",
-        "version": "1.0",
+        "version": "1.1",
         "require_online": False,
 
-        "default_options": {},
+        "default_options": {
+            "model_settings": {
+                "model_path": "",
+                "model_name": "silero.pt",
+                "model_url": "https://models.silero.ai/models/tts/ru/v4_ru.pt"
+            }
+        },
     }
     return manifest
 
 
 async def start_with_options(core: Core, manifest: dict):
-    pass
+    global model_settings
+    model_settings = manifest["options"]["model_settings"]
 
 
 async def _say_silero(core: Core, output_str):
-    global silero_model
+    global silero_model, model_settings
     if is_mute:
         return
     if silero_model is None:  # Подгружаем модель если не подгрузили ранее
         logger.debug("Загрузка модели силеро")
-        if not os.path.isfile("silero.pt"):  # Если нет файла модели - скачиваем
+        # Если нет файла модели - скачиваем
+        if not os.path.isfile(model_settings["model_path"]+model_settings["model_name"]):
             logger.debug("Скачивание модели silero")
-            torch.hub.download_url_to_file("https://models.silero.ai/models/tts/ru/v4_ru.pt", "silero.pt")
+            torch.hub.download_url_to_file(
+                model_settings["model_url"], model_settings["model_path"]+model_settings["model_name"]
+            )
 
-        silero_model = torch.package.PackageImporter("silero.pt").load_pickle("tts_models", "model")
+        silero_model = torch.package.PackageImporter(
+            model_settings["model_path"]+model_settings["model_name"]
+        ).load_pickle("tts_models", "model")
         device = torch.device("cpu")
         silero_model.to(device)
         logger.debug("Загрузка модели силеро завершена")
